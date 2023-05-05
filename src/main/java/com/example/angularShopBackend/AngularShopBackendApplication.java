09b3654f362e4dd2f025ed5c69b1f584fe53b1e5
r134x7 <r134x7@gmx.com>
2023-05-05 23:03:00 +1000

    used spring initializr with Spring WEB and GraphQL dependencies

---
+++ src/main/java/com/example/angularShopBackend/AngularShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularShopBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/angularShopBackend/AngularShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularShopBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
